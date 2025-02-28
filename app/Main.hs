502cf89a4f6629fcef17a8f904ef6a666eb8cf87
barsanges <barsanges@users.noreply.github.com>
2025-02-28 18:33:38 +0100

    Génère la totalité des configurations qui admettent une solution.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {- |
    Module      : Main
    Copyright   : Copyright (C) 2025 barsanges
@@ -8,10 +9,12 @@ Point d'entrée du programme.
 module Main where
 
 import Combinatorics ( tuples )
-import Data.Aeson ( encode, encodeFile )
+import Data.Aeson
 import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map as M
 import qualified Data.Set as S
 import Options.Applicative
+import Explore
 import Puzzle
 
 -- | Le type de sortie.
@@ -53,6 +56,18 @@ allPossibleFences = zero ++ ones ++ twos ++ threes ++ fours
     threes = [ (Just x1, Just x2, Just x3, Nothing) | (x1:x2:x3:_) <- tuples 3 fences ]
     fours = [ (Just x1, Just x2, Just x3, Just x4) | (x1:x2:x3:x4:_) <- tuples 4 fences ]
 
+-- | Un puzzle et le nombre de coups minimal qu'il faut pour le résoudre.
+data Rated = Rated Int Puzzle
+
+instance ToJSON Rated where
+  toJSON = error "toJSON is not implemented for 'Rated'"
+
+  toEncoding (Rated n p) = pairs ( "nmoves" .= n <> "puzzle" .= p )
+
+-- | Renvoie la liste des ancêtres d'un puzzle, avec leurs notes.
+ancestors :: Puzzle -> [Rated]
+ancestors = (fmap (\ (p, n) -> Rated n p)) . M.assocs . (explore (\ _ _ -> False))
+
 -- | Point d'entrée du programme.
 main :: IO ()
 main = do
@@ -61,6 +76,7 @@ main = do
                | s <- allPossibleFigures
                , (mf1, mf2, mf3, mf4) <- allPossibleFences
                ]
+  let solvable = concatMap ancestors solved
   case output of
-    OutputFile f -> encodeFile f solved
-    StdOut -> (BS.putStr . encode) solved
+    OutputFile f -> encodeFile f solvable
+    StdOut -> (BS.putStr . encode) solvable
